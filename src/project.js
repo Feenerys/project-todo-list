738a25e4bad536fd444db201b6b13d58a0720efb
Finn Nguon <36335479+Feenerys@users.noreply.github.com>
2025-07-06 15:52:49 +1000

    feat: add a method to Project to add tasks

--- src/project.js
+++ src/project.js
@@ -1,7 +1,7 @@
 export class Project {
   constructor(title) {
     this.title = title;
-    this.tasks = [];
+    this._tasks = [];
   }
 
   set title(title) {
@@ -9,7 +9,16 @@ export class Project {
       throw new Error("Project must have a valid non-empty title!");
     this._title = title;
   }
+
   get title() {
     return this._title;
+  }
+
+  addTask(task) {
+    this._tasks.push(task);
+  }
+
+  get tasks() {
+    return this._tasks;
   }
 }
