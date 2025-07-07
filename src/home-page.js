801af7e475bc81bc7ecd97688af55f6048069744
Finn Nguon <36335479+Feenerys@users.noreply.github.com>
2025-07-07 15:35:20 +1000

    feat: add function to render tasks and add event listener to projects to render its tasks

--- src/home-page.js
+++ src/home-page.js
@@ -1,15 +1,41 @@
-import "./styles.css";
+export const RenderPage = (projects) => {
+  renderProjects(projects);
 
-export const homePage = (projects) => {
-  projectSection(projects);
+  for (let task of projects[0].tasks){
+    renderTask(task);
+  }
 };
 
-const projectSection = (projects) => {
-  const container = document.querySelector(".project-container");
+const renderProjects = (projects) => {
+  const projectContainer = document.querySelector(".project-container");
+
   for (let project of projects) {
     const projectItem = document.createElement("div");
     projectItem.className = "project";
     projectItem.textContent = project.title;
-    container.appendChild(projectItem);
+
+    projectItem.addEventListener("click", () => {
+      resetTasks()
+      for (let task of project.tasks) {
+        renderTask(task);
+      }
+    });
+
+    projectContainer.appendChild(projectItem);
   }
 };
+
+const renderTask = (task) => {
+  const taskContainer = document.querySelector(".task-container");
+
+  const taskItem = document.createElement("div");
+  taskItem.className = "task";
+  taskItem.textContent = task.title;  
+
+  taskContainer.appendChild(taskItem);
+};
+
+const resetAllTasks = () => {
+  const taskContainer = document.querySelector(".task-container");
+  taskContainer.innerHTML = "";
+}
