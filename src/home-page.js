96a7de10425a31089965aaf646ea0a08bd578b02
Finn Nguon <36335479+Feenerys@users.noreply.github.com>
2025-07-07 16:25:20 +1000

    feat: add more task details to renderTask function and refractored renderProject

--- src/home-page.js
+++ src/home-page.js
@@ -1,28 +1,28 @@
 export const RenderPage = (projects) => {
-  renderProjects(projects);
+  for (let project of projects) {
+    renderProject(project);
+  }
 
-  for (let task of projects[0].tasks){
+  for (let task of projects[0].tasks) {
     renderTask(task);
   }
 };
 
-const renderProjects = (projects) => {
+export const renderProject = (project) => {
   const projectContainer = document.querySelector(".project-container");
 
-  for (let project of projects) {
-    const projectItem = document.createElement("div");
-    projectItem.className = "project";
-    projectItem.textContent = project.title;
+  const projectItem = document.createElement("div");
+  projectItem.className = "project";
+  projectItem.textContent = project.title;
 
-    projectItem.addEventListener("click", () => {
-      resetAllTasks()
-      for (let task of project.tasks) {
-        renderTask(task);
-      }
-    });
+  projectItem.addEventListener("click", () => {
+    resetAllTasks();
+    for (let task of project.tasks) {
+      renderTask(task);
+    }
+  });
 
-    projectContainer.appendChild(projectItem);
-  }
+  projectContainer.appendChild(projectItem);
 };
 
 const renderTask = (task) => {
@@ -30,7 +30,32 @@ const renderTask = (task) => {
 
   const taskItem = document.createElement("div");
   taskItem.className = "task";
-  taskItem.textContent = task.title;  
+
+  const taskStatus = document.createElement("input");
+  taskStatus.type = "checkbox";
+  taskStatus.id = "task-status";
+  taskItem.appendChild(taskStatus);
+
+  const taskTitle = document.createElement("label");
+  taskTitle.className = "task-title";
+  taskItem.appendChild(taskTitle);
+
+  const taskDueDate = document.createElement("div");
+  taskDueDate.className = "task-due-date";
+  taskItem.appendChild(taskDueDate);
+
+  taskTitle.htmlFor = "task-status";
+  taskTitle.textContent = task.title;
+  taskDueDate.textContent = task.dueDate;
+  taskStatus.checked = task.status;
+
+  const taskEditBtn = document.createElement("button");
+  taskEditBtn.textContent = "Edit";
+  taskEditBtn.addEventListener("click", () => {
+    renderTaskEdit();
+  });
+
+  taskItem.appendChild(taskEditBtn);
 
   taskContainer.appendChild(taskItem);
 };
@@ -38,4 +63,6 @@ const renderTask = (task) => {
 const resetAllTasks = () => {
   const taskContainer = document.querySelector(".task-container");
   taskContainer.innerHTML = "";
-}
+};
+
+const renderTaskEdit = () => {};
